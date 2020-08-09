30f7846fce747b032c715aaa01e5ff266e8bb7a7
Shenu Gupta <>
2020-08-09 18:28:40 +0530

    ALL Milk Cell CollectionView Cell Created

---
+++ GrofersApp/AllMilkCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  AllMilkCollectionViewCell.swift
+//  GrofersApp
+//
+//  Created by Shenu Gupta on 09/08/20.
+//  Copyright © 2020 Shenu Gupta. All rights reserved.
+//
+
+import UIKit
+
+class AllMilkCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
