107b488324098ad915eb17b82898ddd9997365fa
Shenu Gupta <>
2020-08-09 18:04:35 +0530

    header of CollectionView Cell created.

---
+++ GrofersApp/HeaderForCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderForCollectionViewCell.swift
+//  GrofersApp
+//
+//  Created by Shenu Gupta on 09/08/20.
+//  Copyright © 2020 Shenu Gupta. All rights reserved.
+//
+
+import UIKit
+
+class HeaderForCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var headerLabelText: UILabel!
+    @IBOutlet weak var viewALLBtn: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
